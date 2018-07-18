b3ad6f406767515d7580d6aacf7777be667949c9
Carlos <clockwerkz@gmail.com>
2018-07-17 22:30:06 -0700

    updates the project to properly use the MVO pattern

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # cat-clicker-premium
-the Premium version of the cat clicker project, utilizing the MVO pattern.
+Premium version of the cat clicker project, utilizing the MVO pattern.
+
+See the live project here: [Cat Clicker Premium](https://clockwerkz.github.io/cat-clicker-premium/)
+
+
+## MVO
+The basis of this assignment was to refactor the previous [Cat Clicker project](https://github.com/clockwerkz/cat-clicker) to using the Model View Octopus Pattern. 
+
+### Model
+The Model object is an array of cat objects, that each contain the properties name, img, altText, clickCount, and selected. Selected is used to highlight the currently selected Cat on the list view object.
+
+### View(s)
+Following the course's suggestion, I created two separate View objects: A List View and a Cat View object. The list view handles the displaying of the ul list of cats on the aside section to the left. The Cat view handles displaying the currently selected Cat in the main section.
+
+### Octopus
+The Octopus object handles calling the list view render when the page first opens (init). It also handles any clicks on the list view, updating the current cat selection to the new clicked cat name, and calling the render for both views with the updated cat. Any clicks on the displayed cat is also handled by the Octopus, incrementing the selectedCat and re-rendering the cat view.
--- js/app.js
+++ js/app.js
@@ -1,60 +1,127 @@
-const catClicker = document.getElementById('cat-clicker');
+/*
+ * Udacity Front End Developer NanoDegree Program
+ * 
+ * Premium Cat Clicker Version 1
+ * 
+ * By Carlos Fins
+ * 
+ * https://github.com/clockwerkz
+ * 
+ * 
+*/
 
+
+// Cat Model Object
 const cats = [
     { name : 'Cleo',
       clickCount : 0,
       altText : 'Kitten Cleo with her siblings',
-      img : 'img/q-aila-162475-unsplash.jpg'},
+      img : 'img/q-aila-162475-unsplash.jpg',
+      selected : false},
       { name : 'Clyde',
       clickCount : 0,
       altText : 'Clyde being lazy on the arm of a couch',
-      img : 'img/sabri-tuzcu-213760-unsplash.jpg'},
+      img : 'img/sabri-tuzcu-213760-unsplash.jpg',
+      selected : false},
       { name : 'Bonnie',
       clickCount : 0,
       altText : 'Bonnie looking up to see if it\'s snack time',
-      img : 'img/bonnie.jpg'},
+      img : 'img/bonnie.jpg',
+      selected : false},
       { name : 'Max',
       clickCount : 0,
       altText : 'Something catches Max\'s eye while on the street',
-      img : 'img/max.jpg'},
+      img : 'img/max.jpg',
+      selected : false},
       { name : 'Noah',
       clickCount : 0,
       altText : 'Noah playing with a string of yarn while on a plastic chair',
-      img : 'img/noah.jpg'}
+      img : 'img/noah.jpg',
+      selected : false}
 ];
 
-cats.sort((a,b) => a.name > b.name );
 
-const catList = document.querySelector('aside .list-of-cats');
-for (let cat of cats) {
-    catList.innerHTML += `<li class='cat-item'>${cat.name}</li>`
-}
-
-catList.addEventListener('click', (e) => {
-    updateDisplay(e.target.textContent);
-});
-
-const updateDisplay = (name) => {
-    let catEntry = document.createElement('div');
-    const cat = cats.filter(cat => cat.name === name)[0];
-    catEntry.innerHTML =  `
-            <div class='entry-body'>
-                <img src='${cat.img}' class='cat-img' alt='${cat.altText}'/>
-                <h2 class='cat-name'>${cat.name}</h2>
-            </div> <!-- .entry-body -->
-            <h2>Clicked: <span class="click-count">${cat.clickCount}</span>
+//List View handles the list of cats on the aside section
+const listView = function() {
+    const catList = document.querySelector('#cat-list .list-of-cats');
     
-    `;
-    catEntry.classList.add('cat-entry');
-    catEntry.addEventListener("click", function(e) {
-        let counter = this.querySelector('.click-count');
-        let newCount = parseInt(counter.innerHTML)+1;
-        const name = this.querySelector('.cat-name').textContent;
-        const cat = cats.filter(cat => cat.name === name)[0];
-        cat.clickCount = newCount;
-        counter.innerHTML = newCount;
+    const render = (cats) => {
+        catList.innerHTML = '';
+        for (let cat of cats) {
+            catList.innerHTML += "<li class='cat-item" + (cat.selected ? " cat-selected" : "") + "'>"+cat.name+"</li>"; 
+        }
+    } 
+
+    catList.addEventListener('click', (e) => {
+        octopus.selectCat(e.target.textContent);
     });
-    catClicker.innerHTML = '';
-    catClicker.appendChild(catEntry);
-}
+
+    return {
+        render
+    };
+}();
+
+
+//Cat View Object displays the selected Cat in the main section
+const catView = function() {
+    const catClicker = document.getElementById('cat-clicker');
+
+    const render = (cat) => {
+        let catEntry = document.createElement('div');
+        catEntry.innerHTML =  `
+                <div class='entry-body'>
+                    <img src='${cat.img}' class='cat-img' alt='${cat.altText}'/>
+                    <h2 class='cat-name'>${cat.name}</h2>
+                </div> <!-- .entry-body -->
+                <h2>Clicked: <span class="click-count">${cat.clickCount}</span>
+        
+        `;
+        catEntry.classList.add('cat-entry');
+        catEntry.addEventListener("click", octopus.clickCounter);
+        catClicker.innerHTML = '';
+        catClicker.appendChild(catEntry);
+    }
+
+    return {
+        render
+    }
+}();
+
+
+// Octopus Object
+const octopus = function(cats) {
+    let selectedCat;
+
+    const init = () => {
+        cats.sort((a,b) => a.name > b.name );
+        listView.render(cats);
+    }
+
+    const selectCat = (name) => {
+        const cat = cats.filter(cat => cat.name === name)[0];
+        if (selectedCat) selectedCat.selected = false;
+        selectedCat = cat;
+        selectedCat.selected = true;
+        catView.render(cat);
+        listView.render(cats);
+
+    }
+
+    const clickCounter = () => {
+        selectedCat.clickCount++;
+        catView.render(selectedCat);
+    }
+
+    return {
+        init,
+        selectCat,
+        clickCounter
+    }
+
+}(cats);
+
+octopus.init();
+
+
+
 
