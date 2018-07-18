30b6b548a60b6a3a605b3f4cca848bc9f7b2e5f2
Carlos <clockwerkz@gmail.com>
2018-07-17 23:33:39 -0700

    refactors model obj to contain the current selected cat vs. residing in the octopus

--- js/app.js
+++ js/app.js
@@ -12,33 +12,42 @@
 
 
 // Cat Model Object
-const cats = [
-    { name : 'Cleo',
-      clickCount : 0,
-      altText : 'Kitten Cleo with her siblings',
-      img : 'img/q-aila-162475-unsplash.jpg',
-      selected : false},
-      { name : 'Clyde',
-      clickCount : 0,
-      altText : 'Clyde being lazy on the arm of a couch',
-      img : 'img/sabri-tuzcu-213760-unsplash.jpg',
-      selected : false},
-      { name : 'Bonnie',
-      clickCount : 0,
-      altText : 'Bonnie looking up to see if it\'s snack time',
-      img : 'img/bonnie.jpg',
-      selected : false},
-      { name : 'Max',
-      clickCount : 0,
-      altText : 'Something catches Max\'s eye while on the street',
-      img : 'img/max.jpg',
-      selected : false},
-      { name : 'Noah',
-      clickCount : 0,
-      altText : 'Noah playing with a string of yarn while on a plastic chair',
-      img : 'img/noah.jpg',
-      selected : false}
-];
+const model = function() {
+    let selectedCat;
+    const cats = [
+        { name : 'Cleo',
+        clickCount : 0,
+        altText : 'Kitten Cleo with her siblings',
+        img : 'img/q-aila-162475-unsplash.jpg',
+        selected : false},
+        { name : 'Clyde',
+        clickCount : 0,
+        altText : 'Clyde being lazy on the arm of a couch',
+        img : 'img/sabri-tuzcu-213760-unsplash.jpg',
+        selected : false},
+        { name : 'Bonnie',
+        clickCount : 0,
+        altText : 'Bonnie looking up to see if it\'s snack time',
+        img : 'img/bonnie.jpg',
+        selected : false},
+        { name : 'Max',
+        clickCount : 0,
+        altText : 'Something catches Max\'s eye while on the street',
+        img : 'img/max.jpg',
+        selected : false},
+        { name : 'Noah',
+        clickCount : 0,
+        altText : 'Noah playing with a string of yarn while on a plastic chair',
+        img : 'img/noah.jpg',
+        selected : false}
+    ];
+
+
+    return {
+        selectedCat,
+        cats
+    }
+}();
 
 
 //List View handles the list of cats on the aside section
@@ -65,9 +74,14 @@ const listView = function() {
 //Cat View Object displays the selected Cat in the main section
 const catView = function() {
     const catClicker = document.getElementById('cat-clicker');
+    const catEntry = catClicker.querySelector('.cat-entry');
+    
+
+    const init = () => {
+        catEntry.addEventListener("click", octopus.clickCounter);
+    }
 
     const render = (cat) => {
-        let catEntry = document.createElement('div');
         catEntry.innerHTML =  `
                 <div class='entry-body'>
                     <img src='${cat.img}' class='cat-img' alt='${cat.altText}'/>
@@ -76,40 +90,39 @@ const catView = function() {
                 <h2>Clicked: <span class="click-count">${cat.clickCount}</span>
         
         `;
-        catEntry.classList.add('cat-entry');
-        catEntry.addEventListener("click", octopus.clickCounter);
-        catClicker.innerHTML = '';
-        catClicker.appendChild(catEntry);
     }
 
     return {
+        init,
         render
     }
 }();
 
 
 // Octopus Object
-const octopus = function(cats) {
-    let selectedCat;
+const octopus = function() {
 
     const init = () => {
-        cats.sort((a,b) => a.name > b.name );
-        listView.render(cats);
+        model.cats.sort((a,b) => a.name > b.name );
+        listView.render(model.cats);
+        catView.init();
     }
 
     const selectCat = (name) => {
-        const cat = cats.filter(cat => cat.name === name)[0];
-        if (selectedCat) selectedCat.selected = false;
-        selectedCat = cat;
-        selectedCat.selected = true;
+        const cat = model.cats.filter(cat => cat.name === name)[0];
+        if (model.selectedCat) model.selectedCat.selected = false;
+        model.selectedCat = cat;
+        model.selectedCat.selected = true;
         catView.render(cat);
-        listView.render(cats);
+        listView.render(model.cats);
 
     }
 
     const clickCounter = () => {
-        selectedCat.clickCount++;
-        catView.render(selectedCat);
+        if (model.selectedCat) {
+            model.selectedCat.clickCount++;
+            catView.render(model.selectedCat);
+        }
     }
 
     return {
@@ -118,7 +131,7 @@ const octopus = function(cats) {
         clickCounter
     }
 
-}(cats);
+}();
 
 octopus.init();
 
