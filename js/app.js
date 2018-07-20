1e6c78916a64bb8c1193c114eaa1a74ba153146e
Carlos <clockwerkz@gmail.com>
2018-07-19 21:44:30 -0700

    completes adding in the admin section

--- js/app.js
+++ js/app.js
@@ -75,17 +75,39 @@ const listView = function() {
 const catView = function() {
     const catClicker = document.getElementById('cat-clicker');
     const catEntry = catClicker.querySelector('.cat-entry');
+    const adminHeader = document.querySelector('.admin-header');
+    const adminElements = document.querySelectorAll('.admin');
+    const adminBtn = document.querySelector('.admin-btn');
+    const cancelBtn = document.querySelector('.cancel-btn');
+    
     
 
-    const init = () => {
+    const init = (cat) => {
+        adminBtn.addEventListener("click", toggleForm);
+        cancelBtn.addEventListener("click", toggleForm);
         catEntry.addEventListener("click", octopus.clickCounter);
-        render();
+        render(cat);
+    }
+
+    const toggleForm = () => {
+        adminElements.forEach((admin) => admin.classList.toggle('admin'));
+        adminHeader.classList.toggle('display');
+        catView.displayForm(octopus.getCurrentCat());
+    }
+
+    const formDisplayed = () => adminHeader.classList.contains('display');
+
+    const displayForm = (cat) => {
+        document.getElementById("cat-name").value = cat.name;
+        document.getElementById("cat-img").value = cat.img;
+        document.getElementById("cat-alt-text").value = cat.altText;
+        document.getElementById("cat-click-count").value = cat.clickCount;
     }
 
     const render = (cat) => {
         catEntry.innerHTML =  `
                 <div class='entry-body'>
-                    <img src='${cat.img}' class='cat-img' alt='${cat.altText}'/>
+                <img src='${cat.img}' class='cat-img' alt='${cat.altText}'/>
                     <h2 class='cat-name'>${cat.name}</h2>
                 </div> <!-- .entry-body -->
                 <h2>Clicked: <span class="click-count">${cat.clickCount}</span>
@@ -95,26 +117,49 @@ const catView = function() {
 
     return {
         init,
-        render
+        render,
+        displayForm,
+        formDisplayed
     }
 }();
 
 
 // Octopus Object
 const octopus = function() {
+    const adminForm = document.querySelector(".admin-form");
+    let currentCat;
 
     const init = () => {
         model.cats.sort((a,b) => a.name > b.name );
-        selectCat(model.cats[0].name);
-        listView.render(model.cats);
-        catView.init();
+        selectCat(model.cats[0].name);      //selects the first cat in the array to display as default
+        listView.render(model.cats);        //renders list of cats after having set first cat as "selected"
+        catView.init(currentCat);        //passes the first cat to the init to render first cat
+        adminForm.addEventListener("submit", (e)=> {
+            e.preventDefault();
+            let name = document.getElementById("cat-name").value;
+            let img = document.getElementById("cat-img").value;
+            let altText = document.getElementById("cat-alt-text").value;
+            let clickCount = document.getElementById("cat-click-count").value;
+            octopus.updateInfo(name, img, altText, clickCount);
+        });
+    }
+
+    const updateInfo = (name, img, altText, clickCount) => {
+        console.log(name, img, altText, clickCount);
+        let index = model.cats.indexOf(currentCat);
+        currentCat = { name : name, img :img, altText : altText, clickCount : clickCount };
+        model.cats[index] = currentCat;
+        catView.render(currentCat);
+        selectCat(currentCat.name);
     }
 
     const selectCat = (name) => {
         const cat = model.cats.filter(cat => cat.name === name)[0];
+        currentCat = cat;
         if (model.selectedCat) model.selectedCat.selected = false;
         model.selectedCat = cat;
         model.selectedCat.selected = true;
+        if (catView.formDisplayed) catView.displayForm(currentCat);
         catView.render(cat);
         listView.render(model.cats);
 
@@ -127,15 +172,20 @@ const octopus = function() {
         }
     }
 
+    const getCurrentCat = () => currentCat;
+
     return {
         init,
         selectCat,
-        clickCounter
+        clickCounter,
+        getCurrentCat,
+        updateInfo
     }
 
 }();
 
-octopus.init();
+document.addEventListener('DOMContentLoaded', octopus.init);
+
 
 
 
